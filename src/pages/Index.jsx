33328417e69d8cfc8585eebe0d72aec4d6871a7b
Hussein Abou Zeinab <husseinabouzeinab@gmail.com>
2024-03-25 03:36:52 +0200

    Fix bug

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,40 +1,42 @@
 import React, { useState } from "react";
-import './Index.css'
+import "./Index.css";
 
-const Counter = ({ cardsList = { count: 1, children: [] }, onAddChild }) => {
-  const [count, setCount] = useState(cardsList.count);
-  const [children, setChildren] = useState(cardsList.children || []);
+const CustomCounter = ({ counterData = { count: 1, children: [] }, onAddChild }) => {
+  const [count, setCount] = useState(counterData.count);
+  const [children, setChildren] = useState(counterData.children || []);
 
   const increment = () => {
     const newChild = { count: count + 1, children: [] };
     setCount(count + 1);
-    setChildren([...children, newChild]); 
+    setChildren([...children, newChild]);
     if (onAddChild) {
       onAddChild(newChild);
     }
   };
 
   const decrement = () => {
-    setChildren(children.filter((child) => child.count!== count));
+    if (count > 1) {
+      setCount(count - 1);
+      setChildren(children.filter((child) => child.count !== count));
+    }
   };
-  
+
   return (
-    <div>
-    <span>- {count}</span>
+    <div className="counter">
+      <span>- 1</span>
       <button onClick={increment}>+</button>
-      
       <button onClick={decrement}>-</button>
       {count > 1 && (
         <ul>
           {children.map((child) => (
             <li key={child.count}>
-              <Counter cardsList={child} onAddChild={onAddChild} />
+              <CustomCounter counterData={child} onAddChild={onAddChild} />
             </li>
           ))}
         </ul>
       )}
     </div>
   );
-}
+};
 
-export default Counter;
+export default CustomCounter;
